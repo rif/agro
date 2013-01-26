4c1cdb944a672c7651663cef1d0dc0831e1d330c
Radu Ioan Fericean <radu@fericean.ro>
2013-01-26 20:57:03 +0200

    better emails

--- applications/agro/controllers/default.py
+++ applications/agro/controllers/default.py
@@ -12,7 +12,7 @@ def status():
         form = crud.update(Status, s.id, next=URL("index"), deletable=False, message=m)
     else:        
         form = crud.create(Status, next=URL("index"),
-                           onaccept=lambda form: mail.send(form.vars.email, "USAB TM", "Puteti edita statusul domneavoastra accesand %s.\n Cu respect,\nUSAMB" % URL('default', 'status', args=form.vars.uuid)), message=m)
+                           onaccept=lambda form: mail.send(form.vars.email, "USAMVBT", "Puteti edita statusul domneavoastra accesand %s.\n Cu respect,\nUSAMB" % URL('default', 'status', args=form.vars.uuid)), message=m)
     response.view = 'default/form.html'
     return locals()
 
@@ -23,7 +23,7 @@ def resume():
         form = crud.update(Resume, r.id, next=URL("index"), deletable=False, message=m)
     else:        
         form = crud.create(Resume, next=URL("index"),
-                           onaccept=lambda form: mail.send(form.vars.email, "USAB TM", "Puteti edita cv-ul domneavoastra accesand %s.\n Cu respect,\nUSAMB" % URL('default', 'resume', args=form.vars.uuid)), message=m)
+                           onaccept=lambda form: mail.send(form.vars.email, "USAMVBT", "Puteti edita cv-ul domneavoastra accesand %s.\n Cu respect,\nUSAMB" % URL('default', 'resume', args=form.vars.uuid)), message=m)
     response.view = 'default/form.html'
     return locals()
 
@@ -40,7 +40,7 @@ def contact_resume():
         if not form.vars.message.strip():
             session.flash = T('Sorry, no message to send!')
         else:
-            if mail.send(resume.email, form.vars.subject, form.vars.message):
+            if mail.send(resume.email, form.vars.subject or 'USAMVBT', form.vars.message):
                 session.flash = T('Thank you, e-mail sent!')
             else:
                 session.flash = T('E-mail sending failed. Please contact the site administration!')
@@ -55,7 +55,7 @@ def job_offer():
         form = crud.update(JobOffer, j.id, next=URL("index"), deletable=False, message=m)
     else:        
         form = crud.create(JobOffer, next=URL("index"),
-                           onaccept=lambda form: mail.send(form.vars.email, "USAB TM", "Puteti edita cv-ul domneavoastra accesand %s.\n Cu respect,\nUSAMB" % URL('default', 'resume', args=form.vars.uuid)), message=m)
+                           onaccept=lambda form: mail.send(form.vars.email, "USAMVBTM", "Puteti edita cv-ul domneavoastra accesand %s.\n Cu respect,\nUSAMB" % URL('default', 'resume', args=form.vars.uuid)), message=m)
     response.view = 'default/form.html'
     return locals()
 
@@ -87,16 +87,16 @@ def recover():
         s = db(Status.email == form.vars.email).select().first()
         if s:
             message += 'Status\n'
-            message += URL('default', 'status', args=s.uuid) + '\n'
+            message += 'http://%s%s\n' %(request.http_host, URL('default', 'status', args=s.uuid))
         r = db(Resume.email == form.vars.email).select().first()
         if r:
             message += 'CV\n'
-            message += URL('default', 'resume', args=r.uuid) + '\n'
+            message += 'http://%s%s\n' %(request.http_host, URL('default', 'resume', args=r.uuid))
         js = db(JobOffer.email == form.vars.email).select()
         if js:
             message += 'Oferte de lucru\n'
             for j in js:
-                message += URL('default', 'job_offer', args=j.uuid) + '\n'
+                message += 'http://%s%s\n' %(request.http_host, URL('default', 'job_offer', args=j.uuid))
 
         if mail.send(form.vars.email, "Link-uri de editare", message):
             response.flash = T('Thank you, e-mail sent!')

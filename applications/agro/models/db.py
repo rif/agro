6c98fffd95ac0ee80fdacfa4e974d1c9f74b3c46
Radu Ioan Fericean <radu@fericean.ro>
2013-01-26 13:24:43 +0200

    added models

--- applications/agro/models/db.py
+++ applications/agro/models/db.py
@@ -1,43 +1,18 @@
 # -*- coding: utf-8 -*-
 
-#########################################################################
-## This scaffolding model makes your app work on Google App Engine too
-## File is released under public domain and you can use without limitations
-#########################################################################
-
 ## if SSL/HTTPS is properly configured and you want all HTTP requests to
 ## be redirected to HTTPS, uncomment the line below:
 # request.requires_https()
 
 if not request.env.web2py_runtime_gae:
-    ## if NOT running on Google App Engine use SQLite or other DB
     db = DAL('sqlite://storage.sqlite',pool_size=1,check_reserved=['all'])
 else:
-    ## connect to Google BigTable (optional 'google:datastore://namespace')
     db = DAL('google:datastore')
-    ## store sessions and tickets there
-    session.connect(request, response, db=db)
-    ## or store session in Memcache, Redis, etc.
-    ## from gluon.contrib.memdb import MEMDB
-    ## from google.appengine.api.memcache import Client
-    ## session.connect(request, response, db = MEMDB(Client()))
 
-## by default give a view/generic.extension to all actions from localhost
-## none otherwise. a pattern can be 'controller/function.extension'
 response.generic_patterns = ['*'] if request.is_local else []
 ## (optional) optimize handling of static files
 # response.optimize_css = 'concat,minify,inline'
 # response.optimize_js = 'concat,minify,inline'
-
-#########################################################################
-## Here is sample code if you need for
-## - email capabilities
-## - authentication (registration, login, logout, ... )
-## - authorization (role based authorization)
-## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
-## - old style crud actions
-## (more options discussed in gluon/tools.py)
-#########################################################################
 
 from gluon.tools import Auth, Crud, Service, PluginManager, prettydate
 auth = Auth(db)
@@ -48,9 +23,9 @@ auth.define_tables(username=False, signature=False)
 
 ## configure email
 mail = auth.settings.mailer
-mail.settings.server = 'logging' or 'smtp.gmail.com:587'
-mail.settings.sender = 'you@gmail.com'
-mail.settings.login = 'username:password'
+mail.settings.server = 'gae'
+#mail.settings.sender = 'you@gmail.com'
+#mail.settings.login = 'username:password'
 
 ## configure auth policy
 auth.settings.registration_requires_verification = False
@@ -62,22 +37,48 @@ auth.settings.reset_password_requires_verification = True
 from gluon.contrib.login_methods.rpx_account import use_janrain
 use_janrain(auth, filename='private/janrain.key')
 
-#########################################################################
-## Define your tables below (or better in another model file) for example
-##
-## >>> db.define_table('mytable',Field('myfield','string'))
-##
-## Fields can be 'string','text','password','integer','double','boolean'
-##       'date','time','datetime','blob','upload', 'reference TABLENAME'
-## There is an implicit 'id integer autoincrement' field
-## Consult manual for more options, validators, etc.
-##
-## More API examples for controllers:
-##
-## >>> db.mytable.insert(myfield='value')
-## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
-## >>> for row in rows: print row.id, row.myfield
-#########################################################################
+if request.uri_language: T.force(request.uri_language)
 
-## after defining tables, uncomment below to enable auditing
-# auth.enable_record_versioning(db)
+import uuid
+
+tracking = db.Table(db, 'tracking',
+                    Field('email', required=True, requires=[IS_EMAIL(), IS_NOT_IN_DB(db, 'status.email')], unique=True),
+                    Field('approved', 'boolean', writable=False, readable=False),
+                    Field('enabled', 'boolean', writable=False, readable=False),
+                    Field('uuid', default=lambda:str(uuid.uuid4()), writable=False, readable=False),
+                    Field('created_on', 'datetime', default=request.now, writable=False, readable=False),
+                    Field('updated_on', 'datetime', update=request.now, writable=False, readable=False),
+)
+
+db._common_fields.append(tracking)
+
+Status = db.define_table('status',
+                         Field('first_name'),
+                         Field('last_name'),                         
+                         Field('phone'),
+                         Field('address', 'text'),
+                         Field('work_place'),
+                         Field('work_field_of_activity'),
+                         Field('work_address', 'text'),
+                         Field('work_position'),
+)
+
+Resume = db.define_table('resume',
+                         Field('first_name'),
+                         Field('last_name'),                         
+                         Field('phone'),
+                         Field('completed_studies'),
+                         Field('competence_areas'),
+                         Field('foreign_languages', 'list:string'),
+)
+
+WorkOffer = db.define_table('work_offer',
+                            Field('company_name'),
+                            Field('city'),
+                            Field('address', 'text'),
+                            Field('position_description', 'text'),
+                            Field('available_position_number'),
+                            Field('offer_expire_date', 'date'),
+)
+
+a0, a1 = request.args(0), request.args(1)
